38fa00378c83bab5287a03699d152411e6154756
Cauã Medeiros <cauamedeiroa3@gmail.com>
2025-03-06 19:00:00 -0300

    Correção de erros

--- Bot.py
+++ Bot.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
 
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
